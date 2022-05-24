902ba37d1d21a73474d02ef49d68b8957f1a74be
Ubuntu <YCCD@YCCD.lp4dhgyfojmundmafonxluc5ea.syx.internal.cloudapp.net>
2022-05-24 01:01:04 +0000

    pyfinish

--- main.py
+++ main.py
@@ -134,3 +134,34 @@ for item in titleList:
 with open("content3.csv", "w") as f:
     w = csv.writer(f)
     w.writerows(result)
+
+
+# 读取数据
+pd.set_option("display.unicode.ambiguous_as_wide", True)
+pd.set_option("display.unicode.east_asian_width", True)
+df = pd.read_csv("content2.csv", names=["path", "title", "date"])
+print(df.head())
+
+# 根据日期进行分组并取数量最多的前10天
+df1 = df.groupby("date").count()["title"].sort_values().tail(10)
+# 查看每天的新闻数量
+print(df1)
+
+import matplotlib.pyplot as plt
+
+
+# 设置中文显示
+plt.rcParams['font.sans-serif'] = ['SimHei']
+plt.rcParams['figure.figsize'] = (10, 5)  # 设置figure_size尺寸
+
+# x轴坐标
+x = df1.index
+# y轴坐标
+y = df1.values
+# 绘制柱状图
+plt.bar(x, y)
+# 设置x轴名称
+plt.xlabel("日期",fontsize=14)
+# 设置x轴名称
+plt.ylabel("数量",fontsize=14)
+plt.show()
